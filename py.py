7fdde415f2fc02da074b4540b8ef88826167c82f
Jozi_Pro <83127529+yosefsahle@users.noreply.github.com>
2023-04-26 09:35:16 -0300

    Update py.py

--- py.py
+++ py.py
@@ -1,10 +1,10 @@
 import os from random import randit
 
 for i in range(1,365):
-for j in range(0, randit(1,10)):
-d = str(i) + 'days ago'
-with open('file.txt;,'a') as file:
-file.write(d)
-os.system('git add .')
-os.system('git commit --date="'+d+'"-m commit"')
+    for j in range(0, randit(1,10)):
+        d = str(i) + 'days ago'
+        with open('file.txt;,'a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit --date="'+d+'"-m commit"')
 os.system('git push -u origin main')
